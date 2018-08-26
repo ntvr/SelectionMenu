81e44fc75d31042b03a2785a6d1203ef66501bc8
Michal Štembera <stembera.michal@gmail.com>
2018-08-26 22:06:22 +0200

    Prepare Example project's dependencies.

--- Example/SelectionMenu/ViewController.swift
+++ Example/SelectionMenu/ViewController.swift
@@ -7,8 +7,34 @@
 //
 
 import UIKit
+import SnapKit
+import SelectionMenu
 
 class ViewController: UIViewController {
+
+    var menuDataSource: SelectionMenuDataSource?
+    weak var menu: SelectionMenu!
+
+    override func loadView() {
+        super.loadView()
+
+        let labelButton = LabelMenuButton(text: "Menu")
+        labelButton.backgroundColor = .red
+        labelButton.label.textColor = .white
+        labelButton.circular = true
+
+        let expandableMenu = SelectionMenu(menuButton: labelButton)
+        view.addSubview(expandableMenu)
+        self.menu = expandableMenu
+
+        menuDataSource = StaticMenuDataSource(sections: [
+            (type: .singleSelection(selected: 0), [.text("A"), .text("B"), .text("C"), .text("D")]),
+            (type: .singleSelection(selected: 1), [.text("1", "2")])
+        ])
+
+        menu.dataSource = menuDataSource
+        menu.delegate = self
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +45,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 }
+
+// MARK: - SelectionMenuDelegate
+extension ViewController: SelectionMenuDelegate {
+    func selectionMenu(_ menu: SelectionMenu, willHide animated: Bool) { }
+
+    func selectionMenu(_ menu: SelectionMenu, willShow animated: Bool) { }
+
+    func selectionMenu(_ menu: SelectionMenu, didSelectSingleIndex index: Int, in section: Int) {
+        print("Single selection at index: \(index) in section: \(section)")
+    }
+
+    func selectionMenu(_ menu: SelectionMenu, didSelectMultipleIndexes indexes: [Int], in section: Int) {
+        print("Multi selection of indexes: \(indexes) in section: \(section)")
+    }
+
+    func selectionMenu(_ menu: SelectionMenu, didSelectButtonAt index: Int, in section: Int) {
+        print("Button at index: \(index) in section: \(section)")
+    }
+}
+
 
