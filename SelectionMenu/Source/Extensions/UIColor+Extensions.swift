0e7791c92fc0d4a3350cd305e4cd95a0fbcb1c72
Michal Štembera <stembera.michal@gmail.com>
2018-09-20 20:09:54 +0200

    BH-5 Add category for adjusting color darkness.

--- SelectionMenu/Source/Extensions/UIColor+Extensions.swift
+++ SelectionMenu/Source/Extensions/UIColor+Extensions.swift
@@ -23,4 +23,26 @@ extension UIColor {
                   blue: hex & 0xFF,
                   alpha: 0xFF)
     }
+
+    func lightened(by percentage: CGFloat = 30.0) -> UIColor? {
+        return self.adjust(by: abs(percentage))
+    }
+
+    func darkened(by percentage: CGFloat = 30.0) -> UIColor? {
+        return self.adjust(by: -1 * abs(percentage))
+    }
+
+    func adjust(by percentage: CGFloat = 30.0) -> UIColor? {
+        let ratio = (0...100).clamp(percentage) / 100
+        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
+
+        if self.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
+            return UIColor(red: min(red + ratio, 1.0),
+                           green: min(green + ratio, 1.0),
+                           blue: min(blue + ratio, 1.0),
+                           alpha: alpha)
+        } else {
+            return nil
+        }
+    }
 }
