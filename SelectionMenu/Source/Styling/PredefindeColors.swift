7b9d5212abcfa947906e6ac764e63b89ef8619b0
Michal Štembera <stembera.michal@gmail.com>
2018-10-01 22:37:22 +0200

    GH-19 Add first four color palettes to be used for predefined menus.

---
+++ SelectionMenu/Source/Styling/PredefindeColors.swift
@@ -0,0 +1,52 @@
+//
+//  PredefindeColors.swift
+//  FBSnapshotTestCase
+//
+//  Created by Michal Štembera on 01/10/2018.
+//
+
+import Foundation
+
+struct CoolBlues {
+    /// #003B46
+    static var deepAqua: UIColor { return UIColor(hex: 0x003B46) }
+    /// #07575B
+    static var ocean: UIColor { return UIColor(hex: 0x07575B) }
+    /// #66A5AD
+    static var wave: UIColor { return UIColor(hex: 0x66A5AD) }
+    /// #C4DFE6
+    static var seafoam: UIColor { return UIColor(hex: 0xC4DFE6) }
+}
+
+struct OutdoorsyAndNatural {
+    /// #2E4600
+    static var forestGreen: UIColor { return UIColor(hex: 0x2E4600) }
+    /// #486B00
+    static var grass: UIColor { return UIColor(hex: 0x486B00) }
+    /// #A2C523
+    static var lime: UIColor { return UIColor(hex: 0xA2C523) }
+    /// #7D4427
+    static var earth: UIColor { return UIColor(hex: 0x7D4427) }
+}
+
+struct FreshAndEnergetic {
+    /// #4CB5F5
+    static var blueSky: UIColor { return UIColor(hex: 0x4CB5F5) }
+    /// #B7B8B6
+    static var granite: UIColor { return UIColor(hex: 0xB7B8B6) }
+    /// #34675C
+    static var pine: UIColor { return UIColor(hex: 0x34675C) }
+    /// #B3C100
+    static var fields: UIColor { return UIColor(hex: 0xB3C100) }
+}
+
+struct DayAndNight {
+    /// #011A27
+    static var darkNaby: UIColor { return UIColor(hex: 0x011A27) }
+    /// #062852
+    static var blueberry: UIColor { return UIColor(hex: 0x062852) }
+    /// #F0810F
+    static var tangerine: UIColor { return UIColor(hex: 0xF0810F) }
+    /// #E6DF44
+    static var daffodil: UIColor { return UIColor(hex: 0xE6DF44) }
+}
