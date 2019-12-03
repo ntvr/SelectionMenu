fcf0ce921879a67b3ebe97ff8fe4da83974e661e
Michal Štembera <stembera.michal@gmail.com>
2019-12-03 17:35:00 +0100

    Expose color definitions within schemes for external usage.

--- SelectionMenu/Source/PredefinedColors.swift
+++ SelectionMenu/Source/PredefinedColors.swift
@@ -11,91 +11,91 @@ import Foundation
 /// [100 brilliant color combinations](https://www.canva.com/learn/100-color-combinations/)
 public struct CoolBlues {
     /// #003B46
-    static var deepAqua: UIColor { return UIColor(hex: 0x003B46) }
+    public static var deepAqua: UIColor { return UIColor(hex: 0x003B46) }
     /// #07575B
-    static var ocean: UIColor { return UIColor(hex: 0x07575B) }
+    public static var ocean: UIColor { return UIColor(hex: 0x07575B) }
     /// #66A5AD
-    static var wave: UIColor { return UIColor(hex: 0x66A5AD) }
+    public static var wave: UIColor { return UIColor(hex: 0x66A5AD) }
     /// #C4DFE6
-    static var seafoam: UIColor { return UIColor(hex: 0xC4DFE6) }
+    public static var seafoam: UIColor { return UIColor(hex: 0xC4DFE6) }
 }
 
 /// Color set based on **Outdoorsy & Natural** color combination from
 /// [100 brilliant color combinations](https://www.canva.com/learn/100-color-combinations/)
 public struct OutdoorsyAndNatural {
     /// #2E4600
-    static var forestGreen: UIColor { return UIColor(hex: 0x2E4600) }
+    public static var forestGreen: UIColor { return UIColor(hex: 0x2E4600) }
     /// #486B00
-    static var grass: UIColor { return UIColor(hex: 0x486B00) }
+    public static var grass: UIColor { return UIColor(hex: 0x486B00) }
     /// #A2C523
-    static var lime: UIColor { return UIColor(hex: 0xA2C523) }
+    public static var lime: UIColor { return UIColor(hex: 0xA2C523) }
     /// #7D4427
-    static var earth: UIColor { return UIColor(hex: 0x7D4427) }
+    public static var earth: UIColor { return UIColor(hex: 0x7D4427) }
 }
 
 /// Color set based on **Fresh & Energetic** color combination from
 /// [100 brilliant color combinations](https://www.canva.com/learn/100-color-combinations/)
 public struct FreshAndEnergetic {
     /// #4CB5F5
-    static var blueSky: UIColor { return UIColor(hex: 0x4CB5F5) }
+    public static var blueSky: UIColor { return UIColor(hex: 0x4CB5F5) }
     /// #B7B8B6
-    static var granite: UIColor { return UIColor(hex: 0xB7B8B6) }
+    public static var granite: UIColor { return UIColor(hex: 0xB7B8B6) }
     /// #34675C
-    static var pine: UIColor { return UIColor(hex: 0x34675C) }
+    public static var pine: UIColor { return UIColor(hex: 0x34675C) }
     /// #B3C100
-    static var fields: UIColor { return UIColor(hex: 0xB3C100) }
+    public static var fields: UIColor { return UIColor(hex: 0xB3C100) }
 }
 
 /// Color set based on **Day & Night** color combination from
 /// [100 brilliant color combinations](https://www.canva.com/learn/100-color-combinations/)
 public struct DayAndNight {
     /// #011A27
-    static var darkNavy: UIColor { return UIColor(hex: 0x011A27) }
+    public static var darkNavy: UIColor { return UIColor(hex: 0x011A27) }
     /// #062852
-    static var blueberry: UIColor { return UIColor(hex: 0x062852) }
+    public static var blueberry: UIColor { return UIColor(hex: 0x062852) }
     /// #F0810F
-    static var tangerine: UIColor { return UIColor(hex: 0xF0810F) }
+    public static var tangerine: UIColor { return UIColor(hex: 0xF0810F) }
     /// #E6DF44
-    static var daffodil: UIColor { return UIColor(hex: 0xE6DF44) }
+    public static var daffodil: UIColor { return UIColor(hex: 0xE6DF44) }
 }
 
 /// Color set based on **Crisp Complementary** color combination from
 /// [100 brilliant color combinations](https://www.canva.com/learn/100-color-combinations/)
 public struct CrispComplementary {
     /// #F4EC6A
-    static var goldenDelicious: UIColor { return UIColor(hex: 0xF4EC6A) }
+    public static var goldenDelicious: UIColor { return UIColor(hex: 0xF4EC6A) }
     /// #BBCF4A
-    static var grannySmith: UIColor { return UIColor(hex: 0xBBCF4A) }
+    public static var grannySmith: UIColor { return UIColor(hex: 0xBBCF4A) }
     /// #E73F0B
-    static var ripeApple: UIColor { return UIColor(hex: 0xE73F0B) }
+    public static var ripeApple: UIColor { return UIColor(hex: 0xE73F0B) }
     /// #A11F0C
-    static var redDelicious: UIColor { return UIColor(hex: 0xA11F0C) }
+    public static var redDelicious: UIColor { return UIColor(hex: 0xA11F0C) }
 }
 
 /// Color set based on **Bold & Basic** color combination from
 /// [100 brilliant color combinations](https://www.canva.com/learn/100-color-combinations/)
 public struct BoldAndBasic {
     /// #000B29
-    static var night: UIColor { return UIColor(hex: 0x000B29) }
+    public static var night: UIColor { return UIColor(hex: 0x000B29) }
     /// #D70026
-    static var phoneBoothRed: UIColor { return UIColor(hex: 0xD70026) }
+    public static var phoneBoothRed: UIColor { return UIColor(hex: 0xD70026) }
     /// #F8F5F2
-    static var pearl: UIColor { return UIColor(hex: 0xF8F5F2) }
+    public static var pearl: UIColor { return UIColor(hex: 0xF8F5F2) }
     /// #EDB83D
-    static var flash: UIColor { return UIColor(hex: 0xEDB83D) }
+    public static var flash: UIColor { return UIColor(hex: 0xEDB83D) }
 }
 
 /// Color set based on **Orange Accent** color combination from
 /// [100 brilliant color combinations](https://www.canva.com/learn/100-color-combinations/)
 public struct OrangeAccent {
     /// #756867
-    static var woodVeener: UIColor { return UIColor(hex: 0x756867) }
+    public static var woodVeener: UIColor { return UIColor(hex: 0x756867) }
     /// #D5D6D2
-    static var sandDollar: UIColor { return UIColor(hex: 0xD5D6D2) }
+    public static var sandDollar: UIColor { return UIColor(hex: 0xD5D6D2) }
     /// #353C3F
-    static var charcoal: UIColor { return UIColor(hex: 0x353C3F) }
+    public static var charcoal: UIColor { return UIColor(hex: 0x353C3F) }
     /// #FF8D3F
-    static var orange: UIColor { return UIColor(hex: 0xFF8D3F) }
+    public static var orange: UIColor { return UIColor(hex: 0xFF8D3F) }
 }
 
 /// Color set based on **Sunny Citrus** color combination from
@@ -115,24 +115,24 @@ public struct SunnyCitrus {
 /// [100 brilliant color combinations](https://www.canva.com/learn/100-color-combinations/)
 public struct FreshGreens {
     /// #265C00
-    static var emerald: UIColor { return UIColor(hex: 0x265C00) }
+    public static var emerald: UIColor { return UIColor(hex: 0x265C00) }
     /// #68A225
-    static var greenBean: UIColor { return UIColor(hex: 0x68A225) }
+    public static var greenBean: UIColor { return UIColor(hex: 0x68A225) }
     /// #B3DE81
-    static var lightGreen: UIColor { return UIColor(hex: 0xB3DE81) }
+    public static var lightGreen: UIColor { return UIColor(hex: 0xB3DE81) }
     /// #FDFFFF
-    static var cotton: UIColor { return UIColor(hex: 0xFDFFFF) }
+    public static var cotton: UIColor { return UIColor(hex: 0xFDFFFF) }
 }
 
 /// Color set based on **Chocolaty Browns** color combination from
 /// [100 brilliant color combinations](https://www.canva.com/learn/100-color-combinations/)
 public struct ChocolatyBrowns {
     /// #301B28
-    static var cocoa: UIColor { return UIColor(hex: 0x301B28) }
+    public static var cocoa: UIColor { return UIColor(hex: 0x301B28) }
     /// #523634
-    static var chocolate: UIColor { return UIColor(hex: 0x523634) }
+    public static var chocolate: UIColor { return UIColor(hex: 0x523634) }
     /// #B6452C
-    static var toffee: UIColor { return UIColor(hex: 0xB6452C) }
+    public static var toffee: UIColor { return UIColor(hex: 0xB6452C) }
     /// #DDC5A2
-    static var frosting: UIColor { return UIColor(hex: 0xDDC5A2) }
+    public static var frosting: UIColor { return UIColor(hex: 0xDDC5A2) }
 }
